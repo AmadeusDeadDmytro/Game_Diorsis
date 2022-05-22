da15dcd224ba27a0d388c9420009ac5379e5b90a
Amadeus <mazurenkodmitriy57@gmail.com>
2022-05-22 14:25:45 +0300

    Check if wall in front of player

--- Assets/_/Code/Scripts/Player/Player.cs
+++ Assets/_/Code/Scripts/Player/Player.cs
@@ -12,9 +12,13 @@ public class Player : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Animator animator;
 
-    [Header("Settings")] [SerializeField] 
-    private WeaponType currentWeapon = WeaponType.None;
-    
+    [Header("Settings")] 
+    [SerializeField] private WeaponType currentWeapon = WeaponType.None;
+
+    [Header("Rays")] 
+    [SerializeField] private GameObject rayPositionLeft;
+    [SerializeField] private GameObject rayPositionRight;
+
     private static readonly int IsFlipping = Animator.StringToHash("isFlipping");
     private Rigidbody2D _rigidbody;
 
@@ -30,9 +34,14 @@ public class Player : MonoBehaviour
 
     public void Movement(float horizontalValue)
     {
+        bool isLeftDirection = horizontalValue < 0;
+        
         // Flip player
-        IsFlip(horizontalValue < 0);
-            
+        IsFlip(isLeftDirection);
+        
+        // Check if wall in front of player
+        if (IsWallInFront(isLeftDirection)) return;
+
         // Move Character
         float moveValue = horizontalValue * playerRunSpeed * Time.deltaTime;
         transform.Translate(Vector3.right * moveValue);
@@ -51,6 +60,21 @@ public class Player : MonoBehaviour
         _rigidbody.velocity = Vector3.zero;
         _rigidbody.angularVelocity = 0.0f;
         animator.SetBool(IsFlipping, false);
+    }
+
+    private bool IsWallInFront(bool isLeft)
+    {
+        RaycastHit2D hit = isLeft
+            ? Physics2D.Raycast(rayPositionLeft.transform.position, Vector2.left) 
+            : Physics2D.Raycast(rayPositionRight.transform.position, Vector2.right);
+
+        if (hit.collider == null) return false;
+        float startRayX = isLeft
+            ? rayPositionLeft.transform.position.x
+            : rayPositionRight.transform.position.x;
+
+        float distance = Mathf.Abs(hit.point.x - startRayX);
+        return distance <= 0.0f;
     }
 
     private void SetCurrentWeapon(WeaponBaseConfig weapon)
